5af86c3a72e6ad887095769cecdf4bd2cc0e0ffe
jiaxianLin <599200100@qq.com>
2017-08-10 10:28:22 +0800

    Initial commit

---
+++ app/src/main/java/com/ht/htlib/App.java
@@ -0,0 +1,8 @@
+package com.ht.htlib;
+
+/**
+ * Created by Administrator on 2017/8/10 0010.
+ */
+
+public class App {
+}
---
+++ htlibrary/src/main/java/com/ht/htlibrary/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.ht.htlibrary;
+
+/**
+ * Created by Administrator on 2017/8/10 0010.
+ */
+
+public class BaseActivity {
+}
